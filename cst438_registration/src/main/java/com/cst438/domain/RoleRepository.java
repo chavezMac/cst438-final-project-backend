7c55be1da59cc52864562cc0039d439861ef30c4
RyRom <109193666+RyRom@users.noreply.github.com>
2023-11-16 15:32:36 -0800

    Update RoleRepository.java
    Added query to get user by name

--- cst438_registration/src/main/java/com/cst438/domain/RoleRepository.java
+++ cst438_registration/src/main/java/com/cst438/domain/RoleRepository.java
@@ -2,6 +2,7 @@ package com.cst438.domain;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface RoleRepository extends CrudRepository <User, Integer> {
 	
@@ -12,4 +13,6 @@ public interface RoleRepository extends CrudRepository <User, Integer> {
 	@Query(value="select * from user", nativeQuery= true)
 	User[] getAllAliases();
 	
+	@Query(value="select * from user where alias = :username", nativeQuery= true)
+	User findByUsername(@Param("username") String username);
 }
